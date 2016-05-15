2c267722767eade7f7dbb43e767b3fbdb2a27d3c
Paul Kernfeld <paulkernfeld@gmail.com>
2016-05-15 11:37:27 +0200

    Debug out info in bin/print-files

--- bin/print-files.js
+++ bin/print-files.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var debug = require('debug')('exandria')
 var argv = require('minimist')(process.argv.slice(2))
 
 var NestLevels = require('../lib/nest-levels')
@@ -10,6 +11,7 @@ var nest = NestLevels(utils.getDbPath(argv.db))
 var setStream = set.SetStream(nest)
 setStream.on('synced', function () {
   for (var fullName in setStream.files) {
+    debug('file', fullName, setStream.files[fullName])
     console.log(fullName)
   }
   process.exit(0)
