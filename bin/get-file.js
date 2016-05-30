befc1a653dfee8f990224a9749a0fb2971071262
Paul Kernfeld <paulkernfeld@gmail.com>
2016-05-30 12:30:43 +0200

    Minimatch for bin/get-file

--- bin/get-file.js
+++ bin/get-file.js
@@ -6,6 +6,7 @@ var path = require('path')
 
 var debug = require('debug')('exandria')
 var hypercore = require('hypercore')
+var minimatch = require('minimatch')
 var argv = require('minimist')(process.argv.slice(2))
 var sprintf = require('sprintf-js').sprintf
 
@@ -18,32 +19,39 @@ var core = hypercore(nest.db('hypercore'))
 
 var filesPath = path.join(utils.getDbPath(argv.db), 'files')
 
-assert.equal(argv._.length, 1)
-var fullName = argv._[0]
+var match = argv._[0] || '*'
 
 var setStream = set.SetStream(nest)
 setStream.once('synced', function () {
-  var fileInfo = setStream.files[fullName]
-  assert(fileInfo, sprintf('File named "%s" not found', fullName))
-
-  var keyHex = fileInfo.hash.toString('hex')
-  var fileFeed = utils.getFileFeed(core, filesPath, keyHex)
-  utils.joinFeedSwarm(fileFeed)
-  fileFeed.get(0, function (err, block) {
-    assert.ifError(err)
-    assert(block)
-    debug('got file', fullName)
-
-    if (!fs.existsSync('./files')) {
-      fs.mkdirSync('./files')
+  var toGet = {}
+  setStream.files.forEach(function (fullName, fileInfo) {
+    if (minimatch(fullName, match, {nocase: true})) {
+      toGet[fullName] = fileInfo
     }
-    var linkPath = path.join('./files', fullName)
-    if (fs.existsSync(linkPath)) {
-      debug('replacing existing file', linkPath)
-      fs.unlinkSync(linkPath)
-    }
-    fs.symlinkSync(path.join('..', filesPath, keyHex), linkPath)
-    process.exit(0)
+  })
+
+  assert(Object.keys(toGet).length > 0, sprintf('No files matched "%s"', match))
+
+  toGet.forEach(function (fullName, fileInfo) {
+    var keyHex = fileInfo.hash.toString('hex')
+    var fileFeed = utils.getFileFeed(core, filesPath, keyHex)
+    utils.joinFeedSwarm(fileFeed)
+    fileFeed.get(0, function (err, block) {
+      assert.ifError(err)
+      assert(block)
+      debug('got file', fullName)
+
+      if (!fs.existsSync('./files')) {
+        fs.mkdirSync('./files')
+      }
+      var linkPath = path.join('./files', fullName)
+      if (fs.existsSync(linkPath)) {
+        debug('replacing existing file', linkPath)
+        fs.unlinkSync(linkPath)
+      }
+      fs.symlinkSync(path.join('..', filesPath, keyHex), linkPath)
+      process.exit(0)
+    })
   })
 })
 
