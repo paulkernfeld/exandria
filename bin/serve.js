d84a2e97c3fef1ebbfdaa2f5423c80a72cf373eb
Paul Kernfeld <paulkernfeld@gmail.com>
2016-07-06 21:38:07 -0400

    Join all feeds for bin/serve.js

--- bin/serve.js
+++ bin/serve.js
@@ -4,6 +4,7 @@ var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
 
+var deasync = require('deasync')
 var hypercore = require('hypercore')
 var argv = require('minimist')(process.argv.slice(2))
 
@@ -12,15 +13,9 @@ var utils = require('../lib/utils')
 
 var nest = NestLevels(utils.getDbPath(argv.db))
 
-var keypair = utils.getKeypair()
 var core = hypercore(nest.db('hypercore'))
 
-var metaFeed = core.createFeed({
-  key: keypair.publicKey,
-  secretKey: keypair.secretKey
-})
-utils.joinFeedSwarm(metaFeed)
-
+var feeds = {}
 var filesPath = path.join(utils.getDbPath(argv.db), 'files')
 if (!fs.existsSync(filesPath)) {
   fs.mkdirSync(filesPath)
@@ -31,9 +26,21 @@ if (!paths) {
 }
 paths.forEach(function (filePath) {
   var fileFeed = utils.getFileFeed(core, filesPath, filePath, {writable: false})
-  fileFeed.get(0, function (err, block) {
-    assert.ifError(err)
-    assert(block)
-    utils.joinFeedSwarm(fileFeed)
+  var block = deasync(fileFeed.get).call(fileFeed, 0)
+  assert(block)
+  feeds[filePath] = fileFeed
+  utils.joinFeedSwarm(fileFeed)
+})
+
+core.list(function (err, feedKeys) {
+  assert.ifError(err)
+  feedKeys.forEach(function (feedKey) {
+    var feedKeyHex = feedKey.toString('hex')
+    if (feeds[feedKeyHex]) {
+      // This is a file feed, and we already joined its swarm above
+      return
+    }
+    var feed = core.createFeed({key: Buffer(feedKeyHex, 'hex')})
+    utils.joinFeedSwarm(feed)
   })
 })
